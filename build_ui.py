a08601027ec6651c3bd32bb5c45354d8882d3518
boyair <yair.zuriel1@gmail.com>
2024-02-18 23:43:08 +0200

    condesed ui window to make it smaller and not take much screen space

--- build_ui.py
+++ build_ui.py
@@ -16,29 +16,29 @@ def __main__():
     buttonbg = "#bbbbbb"  # light grey
     # define window and widgets
     window = tk.Tk()
-    title = tk.Label(window, text="C++ project builder")
+    title = tk.Label(window, text="C++ project\n builder")
     debug_build_btn = tk.Button(window, text="build in debug mode 🔨", command=build_debug)
     release_build_btn = tk.Button(window, text="build in release mode 📤", command=build_release)
     current_os = tk.StringVar(window)
     os_dropdown = tk.OptionMenu(window, current_os, 'linux', 'windows')
     os_menu = window.nametowidget(os_dropdown.menuname)
+    current_os.set(sys.platform)
     # modify main window
-    window.geometry("1240x720")
-    window.config(bg=windowbg)
+    window.geometry("350x520")
+    window.config(bg=windowbg, width=8, height=3)
     window.title('C++ project builder')
     window.resizable(False, False)
     # modify title label
     title.config(font=('David', 35, 'bold'), bg=windowbg, fg='red')
-    title.place(x=300, y=20)
-    debug_build_btn.place(x=200, y=350)
+    title.place(x=30, y=10)
+    debug_build_btn.place(x=10, y=200)
     debug_build_btn.config(bg=buttonbg, font=('David', 20), width=20, height=1)
-    release_build_btn.place(x=600, y=350)
+    release_build_btn.place(x=10, y=300)
     release_build_btn.config(bg=buttonbg, font=('David', 20), width=20, height=1)
     # modify os_dropdown menu
-    current_os.set(sys.platform)
     os_menu.config(font=('David', 20), bg='black', fg='white')
     os_dropdown.config(font=('David', 20), bg='black', fg='white')
-    os_dropdown.place(x=950, y=0)
+    os_dropdown.place(x=0, y=400)
     window.mainloop()
 
 
