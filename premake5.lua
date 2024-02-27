e94781542e9beeee1fdeeb3e940c7b487d52753a
boyair <yair.zuriel1@gmail.com>
2024-02-28 00:36:56 +0200

    changed indetation in premake5 file

--- premake5.lua
+++ premake5.lua
@@ -1,42 +1,41 @@
-
 -- premake5.lua
-workspace "Applicaion"
-	configurations { "Debug", "Release" }
-    architecture "x86_64"
-    cppdialect "C++latest"
+workspace("Applicaion")
 
+configurations({ "Debug", "Release" })
+architecture("x86_64")
+cppdialect("C++latest")
 
-project "Application"
-	kind "ConsoleApp"
-	language "C++"
-    cppdialect "C++latest"
-	targetdir "bin/%{cfg.buildcfg}"
-	 
-	files { "src/*.h", "src/*.cpp" }
+project("Application")
 
-    --link locally installed libraries
-    includedirs{"dependencies/".. os.target() .. "/include"}
-    libdirs{"dependencies/".. os.target() .. "/lib"}
+kind("ConsoleApp")
+language("C++")
+cppdialect("C++latest")
+targetdir("bin/%{cfg.buildcfg}")
 
-if os.target()  == "windows" then
-    print("windows")
-    defines {"_WIN32","WIN32","WINDOWS"}
+files({ "src/*.h", "src/*.cpp" })
+
+--link locally installed libraries
+includedirs({ "dependencies/" .. os.target() .. "/include" })
+libdirs({ "dependencies/" .. os.target() .. "/lib" })
+
+if os.target() == "windows" then
+	print("windows")
+	defines({ "_WIN32", "WIN32", "WINDOWS" })
 elseif os.target() == "linux" then
-    print("linux")
-    defines{"_UNIX","UNIX","LINUX"}
+	print("linux")
+	defines({ "_UNIX", "UNIX", "LINUX" })
 end
 
 -- config for debug build.
-    filter {"configurations:Debug"}
-	 defines { "DEBUG"}
-	 symbols "On"
-	 warnings "Extra"  -- Show extra warnings in  mode
-	    links {}
-
+filter({ "configurations:Debug" })
+defines({ "DEBUG" })
+symbols("On")
+warnings("Extra") -- Show extra warnings in  mode
+links({})
 
 -- config for release build.
-    filter { "configurations:Release"}
-	    defines { "NDEBUG"}
-	    optimize "Full"
-	    flags { "LinkTimeOptimization" } -- Enable link-time optimization
-	    links {}
+filter({ "configurations:Release" })
+defines({ "NDEBUG" })
+optimize("Full")
+flags({ "LinkTimeOptimization" }) -- Enable link-time optimization
+links({})
